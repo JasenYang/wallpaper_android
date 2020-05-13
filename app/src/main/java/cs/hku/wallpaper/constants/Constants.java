93a788bba9e8d2637a2659dfd048d5b21f14e722
杨飞龙 <yangfeilong.beyond@bytedance.com>
2020-05-13 08:48:29 +0800

    update

--- app/src/main/java/cs/hku/wallpaper/constants/Constants.java
+++ app/src/main/java/cs/hku/wallpaper/constants/Constants.java
@@ -1,6 +1,10 @@
 package cs.hku.wallpaper.constants;
 
 public class Constants {
-    public static String IP = "http://192.168.1.117:6789/";
+    public static String IP = "http://192.168.1.105:6789/";
+    /*
+     * If you do not want to run go server on local machine,
+     * please use below config, which can get service from remote server, I deploy base on alibaba cloud
+     */
 //    public static String IP = "http://112.74.183.5:6789/";
 }
